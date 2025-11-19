7e29b09debf9a1ce034e3d6838da0194ad6c0fc0
ravazque <rk.raul1306@gmail.com>
2025-11-19 02:11:28 +0100

    fixes and refactoring

--- module_01/ex06/src/Harl.cpp
+++ module_01/ex06/src/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: ravazque <ravazque@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/18 13:45:47 by ravazque          #+#    #+#             */
-/*   Updated: 2025/11/18 18:29:09 by ravazque         ###   ########.fr       */
+/*   Updated: 2025/11/19 02:08:21 by ravazque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 void	Harl::debug( void )
 {
-	std::cout << "[DEBUG]\nI love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do !" << std::endl;
+	std::cout << "[DEBUG]\nI love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
 }
 
 void	Harl::info( void )
 {
-	std::cout << "[INFO]\nI cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger ! If you did, I wouldn't be asking for more !" << std::endl;
+	std::cout << "[INFO]\nI cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!" << std::endl;
 }
 
 void	Harl::warning( void )
@@ -29,7 +29,7 @@ void	Harl::warning( void )
 
 void	Harl::error( void )
 {
-	std::cout << "[ERROR]\nThis is unacceptable ! I want to speak to the manager now." << std::endl;
+	std::cout << "[ERROR]\nThis is unacceptable! I want to speak to the manager now." << std::endl;
 }
 
 void Harl::complain( string level )
@@ -54,10 +54,13 @@ void Harl::complain( string level )
 	{
         case 0:
             this->debug();
+            __attribute__((fallthrough));
         case 1:
             this->info();
+            __attribute__((fallthrough));
         case 2:
             this->warning();
+            __attribute__((fallthrough));
         case 3:
             this->error();
         break;
